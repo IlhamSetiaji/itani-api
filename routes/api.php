65149054bc3b507098b5815da3643f44dc2403c5
Ilham Setiaji <ilham.ahmadz18@gmail.com>
2022-09-14 13:51:20 +0700

    Revert route when insert pengangkutan

--- routes/api.php
+++ routes/api.php
@@ -106,7 +106,7 @@ Route::post('/ipangan/agronomis/laporan/permintaan/aktif/{pembiayaan_kunjungan_i
 Route::post('/ipangan/agronomis/rab/tambahan', [TestController::class, 'agronomisAddRabTambahan']);
 Route::post('/smartfarming/verifikasi_kegiatan/sidangkomite_mingguan/{pembiayaanID}/{pengajuanID}/{prosesTanamID}', [TestController::class, 'postSidangKomite']);
 Route::post('/smartfarming/verifikasi_kegiatan/sidangkomite_tambahan/{pembiayaanID}/{pengajuanID}', [TestController::class, 'postSidangKomiteTambahan']);
-// Route::post('/ipangan/supir/add/pengangkutan', [TestController::class, 'insertPengangkutan']);
+Route::post('/ipangan/supir/add/pengangkutan', [TestController::class, 'insertPengangkutan']);
 Route::post('/ipangan/supir/update/pengangkutan/{pengangkutanID}', [TestController::class, 'updatePengangkutan']);
 Route::post('/ipangan/supir/add/hasil/panen', [TestController::class, 'addHasilPanen']);
 Route::middleware('auth:sanctum')->group(function () {
