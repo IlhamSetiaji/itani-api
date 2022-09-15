7b328b39625a3546e5e7338a532314214e6cb216
Ilham Setiaji <ilham.ahmadz18@gmail.com>
2022-09-15 10:34:59 +0700

    Added interfaces and repositories, refactor code

---
+++ app/Interfaces/TaskPengajuanInterface.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Interfaces;
+
+interface TaskPengajuanInterface
+{
+    public function create($pengajuanID);
+    public function createSecond($pengajuanID);
+}
---
+++ app/Interfaces/TaskPengajuanProcessInterface.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Interfaces;
+
+interface TaskPengajuanProcessInterface
+{
+    public function create($pengajuanID);
+}
