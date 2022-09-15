89ea98bff292706e33dd1c61777b9b2e466fd437
Ilham Setiaji <ilham.ahmadz18@gmail.com>
2022-09-15 13:46:02 +0700

    Refactor code on PetaniController

---
+++ app/Http/Requests/PembiayaanKunjunganFileRequest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Helpers\ResponseFormatter;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Contracts\Validation\Validator as ValidationValidator;
+
+class PembiayaanKunjunganFileRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'file_id' => 'required|numeric',
+            'pembiayaan_kunjungan_id' => 'required|string',
+            'file_path' => 'required|string',
+            'file_img' => 'required|string',
+        ];
+    }
+
+    protected function failedValidation(ValidationValidator $validator)
+    {
+        throw new HttpResponseException(ResponseFormatter::error($validator, $validator->messages(), 417));
+    }
+}
